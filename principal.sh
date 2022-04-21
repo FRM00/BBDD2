c684fcef16cae5ae0a24bab892f78bbc44afece4
FRM00 <782891@unizar.es>
2022-04-21 18:22:30 +0200

    script

--- principal.sh
+++ principal.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 echo "Descargamos los archivos"
-./descargarArchivos.sh
-#crearUserBD 
+#./descargarArchivos.sh
+
+echo "Archivos descargados"
+echo " "
+echo "Comenzamos con la creacion de la BD y el usuario"
+echo " "
 #Ejecutamos sql para crear la BD y el usuario
+#sudo -iu postgres psql < crearUserBD.sql
 
-
+#Cargamos los datos en la BD de los archivos csv
+sudo -iu postgres
+#psql -U responsablep1 -d Practica1 -a < cargarDatos.sql
