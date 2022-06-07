a402d55285abc3b1c4c12954cd3764b7181efec2
FRM00 <782891@unizar.es>
2022-06-07 11:51:58 +0200

    script

--- principal.sh
+++ principal.sh
@@ -5,14 +5,3 @@ echo "Descargamos los archivos"
 
 echo "Archivos descargados"
 echo " "
-echo "Comenzamos con la creacion de la BD y el usuario"
-echo " "
-#Ejecutamos sql para crear la BD y el usuario
-#sudo -iu postgres psql < crearUserBD.sql
-
-#Cargamos los datos en la BD de los archivos csv
-#sudo -iu postgres
-cd /home/alumno/Desktop/Prueba
-psql -U responsablep1 -d Practica1 < cargarDatos.sql
-echo "Lanzamos el trigger"
-psql -U responsablep1 -d Practica1 < trigger.sql
