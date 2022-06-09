c753b41dd0ef99c3e0bfee7b18fa774533e8046f
Revi00 <73936133+Revi00@users.noreply.github.com>
2022-06-09 14:11:13 +0100

    f

--- Practica_3/principal.sh
+++ Practica_3/principal.sh
@@ -5,7 +5,7 @@ echo " "
 
 cd /home/alumno/Desktop/Prueba/Practica_3
 
-sudo -iu postgres psql < crearUserBD.sql
+sudo -iu postgres psql < crearBD.sql
 
 psql -U postgres -d Practica3 -a < pregunta1.sql
 
