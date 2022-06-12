6a9458d3cc7f383ee2696e32be5bd052b57012d1
FRM00 <80319243+FRM00@users.noreply.github.com>
2022-06-12 10:54:00 +0200

    final p3

--- Practica_3/pregunta4.sql
+++ Practica_3/pregunta4.sql
@@ -19,23 +19,23 @@ WHERE apellido1 LIKE 'Gracia';
 
 
 ---Consulta 5
-SELECT i.codAsig, a.nombreAsig
+SELECT a.nombreAsig
 FROM Asignatura a, Imparte i
 WHERE 
-    a.codAsig = i.codAsig
-GROUP BY i.codAsig, a.nombreAsig
-HAVING COUNT(i.DNI) > 1;
+    a.codAsig = i.codigo_asignatura
+GROUP BY a.nombreAsig
+HAVING COUNT(i.dni_docente) > 1;
 
 
 ---Consulta 6
-SELECT d.dni, d.nombre, d.apellido1, COUNT(i.codigo_asignatura) AS CargaTrabajoDocent
+SELECT d.dni, COUNT(i.codigo_asignatura) as CargaTrabajoDocent
 FROM Docente d, Imparte i
 WHERE d.dni = i.dni_docente
 GROUP BY d.dni
 
 UNION
 
-SELECT i.dni, i.nombre, i.apellido1, COUNT(p.referencia_proyecto) AS CargaTrabajoInvest
+SELECT i.dni, COUNT(p.referencia_proyecto) as CargaTrabajoInvest
 FROM Investigador i, Participa p
 WHERE i.dni = p.dni_investigador
-GROUP BY i.dni
+GROUP BY i.dni;
